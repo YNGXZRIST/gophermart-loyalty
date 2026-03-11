b09fbe6eb126f3955c90b2dfa2e60a1408d2f87a
Vasily Kulagin <vascool66@gmail.com>
2026-03-11 21:48:58 +0200

    смена приоритетов флагов над переменными окружения

--- internal/gopherman/config/server/internal/flags.go
+++ internal/gopherman/config/server/internal/flags.go
@@ -16,28 +16,29 @@ type Options struct {
 }
 
 func NewOptions(args []string) (*Options, error) {
-	opt, err := parseArgs(args)
-	if err != nil {
-		return nil, fmt.Errorf("ERROR: cannot parse arguments: %w", err)
-	}
-	err = opt.parseEnv()
+	opt := new(Options)
+	err := opt.parseEnv()
 	if err != nil {
 		return nil, fmt.Errorf("ERROR: cannot parse environment variables: %w", err)
 	}
+	err = opt.parseArgs(args)
+	if err != nil {
+		return nil, fmt.Errorf("ERROR: cannot parse arguments: %w", err)
+	}
+
 	return opt, nil
 }
-func parseArgs(args []string) (*Options, error) {
+func (opt *Options) parseArgs(args []string) error {
 	flags := flag.NewFlagSet("server", flag.ContinueOnError)
-	opt := new(Options)
 	flags.StringVar(&opt.Address, "a", "localhost", "Address of the server")
 	flags.StringVar(&opt.DatabaseURL, "d", "", "Database URL")
 	flags.StringVar(&opt.AccrualAddress, "r", "", "Address of accrual server")
 	flags.StringVar(&opt.Mode, "m", constant.TypeModeDefault, "Server mode")
 	err := flags.Parse(args)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	return opt, nil
+	return nil
 }
 func (opt *Options) parseEnv() error {
 	err := env.Parse(opt)
