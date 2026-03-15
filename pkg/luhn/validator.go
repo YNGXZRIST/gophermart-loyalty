d5db6357e4ba26892e3c1987dc8602401ec9d6db
Vasily Kulagin <vascool66@gmail.com>
2026-03-15 11:41:30 +0200

    валидация номера заказа алгоритмом Луна

---
+++ pkg/luhn/validator.go
@@ -0,0 +1,37 @@
+package luhn
+
+import (
+	"strconv"
+	"strings"
+)
+
+func Validate(str string) bool {
+	str = strings.TrimSpace(str)
+	str = strings.ReplaceAll(str, " ", "")
+	if _, err := strconv.Atoi(str); err != nil {
+		return false
+	}
+	runes := []rune(str)
+	nums := make([]int, 0, len(runes))
+	for _, v := range runes {
+		n, err := strconv.Atoi(string(v))
+		if err != nil {
+			return false
+		}
+		nums = append(nums, n)
+	}
+	sum := 0
+	for i, d := range nums {
+		if (len(nums)-1-i)%2 == 1 {
+			d *= 2
+			if d > 9 {
+				d -= 9
+			}
+		}
+		sum += d
+	}
+	if sum%10 == 0 {
+		return true
+	}
+	return false
+}
